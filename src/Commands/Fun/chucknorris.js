f6ee0c70194dd9676766777a88a98c25b6558788
BaumianerNiklas <niklas3705@gmail.com>
2020-08-17 21:05:25 +0200

    added chucknorris command

---
+++ src/Commands/Fun/chucknorris.js
@@ -0,0 +1,18 @@
+const {
+	default: { get },
+} = require("axios");
+const { randomArr } = require("../../Utilities/functions");
+
+module.exports.run = async (bot, msg, args) => {
+	const response = await get("https://api.chucknorris.io/jokes/random");
+	msg.channel.send(response.data.value);
+};
+
+module.exports.help = {
+	name: "chucknorris",
+	aliases: ["norris", "chuck"],
+	category: "Fun",
+	description: "Get random chuck norris joke.",
+	usage: "trivia",
+	example: "trivia",
+};

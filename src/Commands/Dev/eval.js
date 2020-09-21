deb0bb325d7c8c003941dacd73e37414eb29b7a3
BaumianerNiklas <niklas3705@gmail.com>
2020-09-21 20:48:05 +0200

    fixed results with too many characters

--- src/Commands/Dev/eval.js
+++ src/Commands/Dev/eval.js
@@ -3,45 +3,43 @@ const consts = require("../../Utilities/constants.js");
 const Discord = require("discord.js");
 const fs = require("fs");
 
-function clean (text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
-    else
-        return text;
-  }
-
+function clean(text) {
+	if (typeof text === "string")
+		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+	else return text;
+}
 
 module.exports.run = async (bot, msg, args) => {
-    if(msg.author.id !== process.env.OWNER_ID) return;
-    let result;
+	if (msg.author.id !== process.env.OWNER_ID) return;
+	let result;
 
-    try {
-      
-      const code = args.join(" ");
-      result = eval(code);
- 
-      if (typeof evaled !== "string") {
-        result = require("util").inspect(result); 
-       }
-       result = clean(result);
+	try {
+		const code = args.join(" ");
+		result = eval(code);
 
-    } catch (err) {
-        result = clean(err);
-    }
-    let embed = new Discord.MessageEmbed()
-    .setTitle("Eval")
-    .setTimestamp()
-    .setColor(consts.orange)
-    .setDescription("```xs\n"+result+"```");
-    msg.channel.send(embed);
-  }
+		if (typeof evaled !== "string") {
+			result = require("util").inspect(result);
+		}
+		result = clean(result);
+	} catch (err) {
+		result = clean(err);
+	}
 
+	let embed = new Discord.MessageEmbed().setTitle("Eval").setTimestamp().setColor(consts.orange);
+	if (result.length > 2048) {
+		const haste = await fcs.haste(result);
+		embed.setDescription(`Result exceeded the 2048 character limit. [[Hastebin Link]](${haste})`);
+	} else {
+		embed.setDescription("```xs\n" + result + "```");
+	}
+	msg.channel.send(embed);
+};
 
 module.exports.help = {
-    name: "eval",
-    aliases: ["e", "ev"],
-    category: "Dev",
-    description: "Eval some code",
-    usage: "eval <code>",
-    example: "eval 1+1" 
-}
+	name: "eval",
+	aliases: ["e", "ev"],
+	category: "Dev",
+	description: "Eval some code",
+	usage: "eval <code>",
+	example: "eval 1+1",
+};

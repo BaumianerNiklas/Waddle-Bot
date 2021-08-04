fb40467c8a4da5eb9c62b587b369b29eee022a0a
BaumianerNiklas <niklas3705@gmail.com>
2021-08-04 17:45:39 +0200

    chore: clean project setup yikes.. empty index.ts file so that TypeScript doesn't complain

--- src/Events/guildDelete.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (bot, guild) => {
-	console.log(`${bot.user.username} has been removed from a guild!`);
-	console.log(
-		`Name: ${guild.name} | ID: ${guild.id} | Member Count: ${guild.memberCount}\nOwner: ${guild.owner.user.tag} (${guild.ownerID})`
-	);
-};

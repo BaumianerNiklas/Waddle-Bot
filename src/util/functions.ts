ea90ed68f4b1bb576cad6fb1bbb274bae2c28f0a
BaumianerNiklas <niklas3705@gmail.com>
2021-08-10 17:44:08 +0200

    feat(util): add shuffleArray

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,5 +1,8 @@
-// TODO: better name
+export function shuffleArray<T>(array: T[]): T[] {
+	return array.sort(() => Math.random() - 0.5);
+}
 
+// TODO: better name
 type DiscordTimestampFormatStyle = "t" | "T" | "d" | "D" | "f" | "F" | "R";
 export function formatUnixTimestamp(unixTimestamp: number, style: DiscordTimestampFormatStyle = "f"): string {
 	return `<t:${Math.floor(unixTimestamp / 1000)}:${style}>`;

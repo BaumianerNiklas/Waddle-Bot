573ee7f7171bafa335572255b506d73661bb09c1
BaumianerNiklas <niklas3705@gmail.com>
2021-08-12 13:14:56 +0200

    feat(constants): add TESTING_GUILD

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,5 @@
 import { Snowflake } from "discord-api-types";
 
 export const APPLICATION_ID = "723224456671002674" as Snowflake;
+export const TESTING_GUILD = "859164137187967006" as Snowflake;
 export const BOT_COLOR = 0xf39c12;

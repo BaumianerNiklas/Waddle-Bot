6ab730318877be413eedcb5035b69bd86d4a10f8
BaumianerNiklas <niklas3705@gmail.com>
2021-08-16 17:08:11 +0200

    fix: better handling of retrieving commands

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -12,7 +12,11 @@ export default class extends BaseEvent {
 
 	async run(bot: WaddleBot, interaction: Interaction) {
 		if (!interaction.isCommand() && !interaction.isContextMenu()) return;
-		const command = bot.commandHandler.commands.get(interaction.commandName)!;
+		const command = bot.commandHandler.commands.get(interaction.commandName);
+
+		if (!command) {
+			return interaction.reply({ content: "This command has not yet been implemented!", ephemeral: true });
+		}
 
 		if (command.guildOnly && !interaction.guildId) {
 			return interaction.reply("Sorry, this command doesn't work in DM's. Try running it on a server!");

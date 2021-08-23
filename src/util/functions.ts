3dd1e1b68b72a2e6d066e16a60f771c54dc1e002
BaumianerNiklas <niklas3705@gmail.com>
2021-08-23 18:11:21 +0200

    feat(functions): add generateMessageLink

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,9 +1,15 @@
+import type { Message } from "discord.js";
+
 export function capitalizeFirstLetter(text: string) {
 	return text.replace(/(?<![^ \n\t])[a-z]/g, (char) => char.toUpperCase());
 }
 
 export function shuffleArray<T>(array: T[]): T[] {
 	return array.sort(() => Math.random() - 0.5);
+}
+
+export function generateMessageLink(message: Message): string {
+	return `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
 }
 
 // TODO: better name

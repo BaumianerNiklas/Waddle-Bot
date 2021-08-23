1c02697da32418a96c1f4a78c87aae5eae09b158
BaumianerNiklas <niklas3705@gmail.com>
2021-08-23 18:10:26 +0200

    feat(constants): add field and clock emote
    + some comments for organization

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,11 +1,18 @@
 import type { Snowflake } from "discord-api-types";
 
+// Metainformation/Snowflakes
 export const BOT_OWNER_ID = "337588047111520257" as Snowflake;
 export const APPLICATION_ID = "723224456671002674" as Snowflake;
 export const TESTING_GUILD = "859164137187967006" as Snowflake;
 
+// Colors
 export const BOT_COLOR = 0xf39c12;
 
+// Emotes
+export const FIELD_EMOTE = "<:field:879323600635699240>";
+export const ORANGE_CLOCK_EMOTE = "<:clock:879366314077089842>";
+
+// Utilities
 export const COMMAND_OPTION_TYPES: Record<string, number> = {
 	SUB_COMMAND: 1,
 	SUB_COMMAND_GROUP: 2,

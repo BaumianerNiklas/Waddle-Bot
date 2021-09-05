d3e43a9f015a27e13b4afb7d0c9fd29e93d91cd7
BaumianerNiklas <niklas3705@gmail.com>
2021-09-05 14:10:58 +0200

    refactor(deps): use node-fetch instead of axios

--- src/commands/Fun/inspirobot.ts
+++ src/commands/Fun/inspirobot.ts
@@ -1,5 +1,5 @@
 import { BaseCommand } from "#structures/BaseCommand.js";
-import axios from "axios";
+import fetch from "node-fetch";
 import { CommandInteraction, MessageEmbed } from "discord.js";
 
 export class Command extends BaseCommand {
@@ -14,16 +14,16 @@ export class Command extends BaseCommand {
 	async run(int: CommandInteraction) {
 		await int.deferReply();
 
-		const result = await axios.get("https://inspirobot.me/api?generate=true");
+		const result = await (await fetch("https://inspirobot.me/api?generate=true")).text();
 
 		const embed = new MessageEmbed()
 			.setAuthor(
 				"Get Inspired!",
 				"https://pbs.twimg.com/profile_images/815624354876760064/zPmAZWP4_400x400.jpg",
-				result.data
+				result
 			)
 			.setColor(0xa890b)
-			.setImage(result.data);
+			.setImage(result);
 
 		int.editReply({ embeds: [embed] });
 	}
--- src/commands/Fun/meme.ts
+++ src/commands/Fun/meme.ts
@@ -1,7 +1,7 @@
 import { BaseCommand, CommandData } from "#structures/BaseCommand.js";
 import { randomItemFromArray } from "#util/functions.js";
-import axios from "axios";
 import { CommandInteraction, MessageEmbed } from "discord.js";
+import fetch from "node-fetch";
 
 const subreddits = ["dankmemes", "memes", "meme", "me_irl", "antimeme", "BlackPeopleTwitter", "WhitePeopleTwitter"];
 
@@ -28,8 +28,8 @@ export class Command extends BaseCommand {
 
 		const choice = int.options.getString("subreddit");
 		const subreddit = choice ?? randomItemFromArray(subreddits);
-		const { data }: { data: RedditData } = await axios.get(`https://reddit.com/r/${subreddit}/hot.json`);
-
+		const result = await fetch(`https://reddit.com/r/${subreddit}/hot.json`);
+		const data = (await result.json()) as RedditData;
 		const posts = data.data.children.filter((p) => {
 			return !p.data.pinned && !p.data.stickied && !p.data.is_gallery && !p.data.is_video;
 			// Pinned/Stickied posts are mostly metaposts about the subreddit

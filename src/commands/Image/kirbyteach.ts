1f00602522a39ea92c3710519eb0b0796af578c4
BaumianerNiklas <niklas3705@gmail.com>
2021-09-09 12:51:07 +0200

    feat(commands): add kirbyteach.ts

---
+++ src/commands/Image/kirbyteach.ts
@@ -0,0 +1,41 @@
+import { BaseCommand, CommandData } from "#structures/BaseCommand.js";
+import { CommandInteraction, MessageAttachment } from "discord.js";
+import Canvas from "canvas";
+import { chunkString } from "#util/functions.js";
+const { createCanvas, loadImage } = Canvas;
+
+@CommandData({
+	name: "kirbyteach",
+	description: "Let Kirby teach something!",
+	category: "Image",
+	options: [
+		{
+			type: "STRING",
+			name: "text",
+			description: "The text Kirby should teach",
+			required: true,
+		},
+	],
+})
+export class Command extends BaseCommand {
+	async run(int: CommandInteraction) {
+		await int.deferReply();
+		const text = int.options.getString("text", true);
+
+		const canvas = createCanvas(1000, 750);
+		const ctx = canvas.getContext("2d");
+
+		const background = await loadImage("./Assets/kirbyteach.jpeg");
+		ctx.drawImage(background, 0, 0, 1000, 750);
+
+		ctx.font = "32px Arial";
+		let teachText = chunkString(text, 30).join("\n");
+		if (teachText.length >= 330) {
+			teachText = teachText.slice(0, 327) + "...";
+		}
+		ctx.fillText(teachText, 150, 150, 515);
+
+		const attachment = new MessageAttachment(canvas.toBuffer(), "kirbyteach.jpeg");
+		int.editReply({ files: [attachment] });
+	}
+}

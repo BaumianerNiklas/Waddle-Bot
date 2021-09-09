233d28cd62aae0948d98505f1ddb76562f5525c1
BaumianerNiklas <niklas3705@gmail.com>
2021-09-09 19:26:45 +0200

    feat(commands): add beautiful.ts

---
+++ src/commands/Image/beautiful.ts
@@ -0,0 +1,40 @@
+import { BaseCommand, CommandData } from "#structures/BaseCommand.js";
+import { CommandInteraction, MessageAttachment } from "discord.js";
+import Canvas from "canvas";
+const { createCanvas, loadImage } = Canvas;
+
+@CommandData({
+	name: "beautiful",
+	description: "Show everyone that you (or someone else) is beautiful!",
+	category: "Image",
+	options: [
+		{
+			type: "USER",
+			name: "user",
+			description: "The user that should be portrayed as beautiful",
+		},
+	],
+})
+export class Command extends BaseCommand {
+	async run(int: CommandInteraction) {
+		// TODO: support for uploading attachments once a attachments option releases for interactions
+		await int.deferReply();
+		const image = (int.options.getUser("user") ?? int.user).displayAvatarURL({ format: "jpg" });
+
+		const canvas = createCanvas(623, 655);
+		const ctx = canvas.getContext("2d");
+
+		const background = await loadImage("./assets/beautiful.jpeg");
+		ctx.drawImage(background, 0, 0, 623, 655);
+
+		ctx.rotate(-0.02);
+		const beautiful1 = await loadImage(image);
+		ctx.drawImage(beautiful1, 422, 49, 150, 177.5);
+
+		const beautiful2 = await loadImage(image);
+		ctx.drawImage(beautiful2, 418.5, 378, 150, 177.5);
+
+		const result = new MessageAttachment(canvas.toBuffer(), "beautiful.png");
+		int.editReply({ files: [result] });
+	}
+}

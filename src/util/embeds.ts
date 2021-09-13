07ecc18a22c684ca8af864ef502004b6d2c510ce
BaumianerNiklas <niklas3705@gmail.com>
2021-09-13 11:21:23 +0200

    feat(embds): add SuccessEmbed

--- src/util/embeds.ts
+++ src/util/embeds.ts
@@ -1,5 +1,24 @@
 import { MessageEmbed, MessageEmbedOptions } from "discord.js";
-import { COLOR_RED, EMOTE_DANGER } from "./constants.js";
+import { COLOR_GREEN, COLOR_RED, EMOTE_DANGER, EMOTE_GREEN_TICK } from "./constants.js";
+
+export class SuccessEmbed extends MessageEmbed {
+	constructor(data?: MessageEmbed | MessageEmbedOptions | string) {
+		if (typeof data === "string") {
+			super({ description: SuccessEmbed.formatDescription(data) });
+			this.color = COLOR_GREEN;
+		} else {
+			super(data);
+			this.color = COLOR_GREEN;
+			if (data?.description) {
+				this.description = SuccessEmbed.formatDescription(data.description);
+			}
+		}
+	}
+
+	private static formatDescription(text: string): string {
+		return `${EMOTE_GREEN_TICK} ${text}`;
+	}
+}
 
 export class ErrorEmbed extends MessageEmbed {
 	constructor(data?: MessageEmbed | MessageEmbedOptions | string) {

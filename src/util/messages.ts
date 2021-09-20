cb51cdde6b934ef4c59035c40508cc976c66a40f
BaumianerNiklas <niklas3705@gmail.com>
2021-09-20 18:57:20 +0200

    refactor(messages): add as const to messages

--- src/util/messages.ts
+++ src/util/messages.ts
@@ -1,7 +1,8 @@
-export const SUCCESS = (message: string) => `Sucessfully ${message}`;
-export const FETCHING_API_FAILED = (message: string) => `Sorry, something went wrong while trying to fetch ${message}.`;
+export const SUCCESS = (message: string) => `Sucessfully ${message}` as const;
+export const FETCHING_API_FAILED = (message: string) =>
+	`Sorry, something went wrong while trying to fetch ${message}.` as const;
 
 export const EMOTE_NOT_ON_SERVER = (emoteName?: string) => {
-	if (emoteName) return `The emote **${emoteName}** doesn't appear to be on this server.`;
-	else return "That emote doesn't appear to be on this server.";
+	if (emoteName) return `The emote **${emoteName}** doesn't appear to be on this server.` as const;
+	else return "That emote doesn't appear to be on this server." as const;
 };

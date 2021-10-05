6dab6aa1232545ce01cf175009482a94a1a3a142
BaumianerNiklas <niklas3705@gmail.com>
2021-10-05 19:41:18 +0200

    feat(commands/info): major improvements
    * simplify timestamps to include only relative time
    * defer reply
    
    * user info improvements:
    * * use accent color, if available, for embed color
    * * typecheck that member.joinedTimestamp is non-null
    * * display guild avatar
    * * move tag and standard (non-guild) avatar to author in embed
    * * display permissions
    
    * role info improvements:
    * * more type safety
    * * fix position field
    * * display permissions
    * * display role icon, if exists
    
    * server info improvements:
    * * more type safety
    * * display banner, if exists

--- src/commands/Utility/info.ts
+++ src/commands/Utility/info.ts
@@ -1,6 +1,9 @@
 import { BaseCommand } from "#structures/BaseCommand.js";
-import { discordTimestamp } from "#util/functions.js";
-import { CommandInteraction, Guild, GuildMember, MessageEmbed, Role } from "discord.js";
+import { ErrorEmbed } from "#util/embeds.js";
+import { capitalizeFirstLetter, discordTimestamp } from "#util/functions.js";
+import { ImageURLOptions } from "@discordjs/rest";
+import { APIRole } from "discord-api-types";
+import { CommandInteraction, Guild, GuildMember, MessageEmbed, Role, Permissions } from "discord.js";
 
 export class Command extends BaseCommand {
 	constructor() {
@@ -44,61 +47,92 @@ export class Command extends BaseCommand {
 	}
 
 	async run(int: CommandInteraction) {
+		await int.deferReply();
 		const subcommand = int.options.getSubcommand(true);
+		const avatarOptions: ImageURLOptions = { dynamic: true, size: 256 };
 
 		if (subcommand === "user") {
 			const member = (int.options.getMember("user") as GuildMember) ?? (int.member as GuildMember);
+			// Users have to be force fetched in order for their accent (banner) color to be present in the data
+			const user = await int.client.users.fetch(member.id, { force: true });
+
+			const joinedAt = member.joinedTimestamp
+				? discordTimestamp(member.joinedTimestamp, "R")
+				: "This user appears to have left the server.";
 
 			const embed = new MessageEmbed()
-				.setTitle(member.user.tag)
-				.setColor(member.roles.highest.color)
-				.setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
-				.addField(
-					"Joined At",
-					`${discordTimestamp(member.joinedTimestamp!)} (${discordTimestamp(member.joinedTimestamp!, "R")})`
-				)
-				.addField(
-					"Created At",
-					`${discordTimestamp(member.user.createdTimestamp!)} (${discordTimestamp(
-						member.user.createdTimestamp!,
-						"R"
-					)})`
-				)
+				.setAuthor(user.tag, user.displayAvatarURL(avatarOptions))
+				.setColor(user.accentColor ?? member.roles.highest.color)
+				.setThumbnail(member.displayAvatarURL(avatarOptions))
+				.addField("Joined At", joinedAt, true)
+				.addField("Created At", discordTimestamp(user.createdTimestamp, "R"), true)
+				.addField("Permissions", this.formatPermissions(member.permissions))
 				.setFooter(`ID: ${member.id}`);
 
-			return int.reply({ embeds: [embed] });
+			return int.editReply({ embeds: [embed] });
 		} else if (subcommand === "role") {
-			const role = int.options.getRole("role", true) as Role;
+			let role: Role | APIRole | null = int.options.getRole("role", true);
+			if (!(role instanceof Role)) {
+				role = await int.guild!.roles.fetch(role.id);
+			}
+
+			if (!role) {
+				return int.editReply({
+					embeds: [
+						new ErrorEmbed(
+							"The supplied role doesn't appear to be on this server. Make sure it exists and is not deleted!"
+						),
+					],
+				});
+			}
 
 			const embed = new MessageEmbed()
 				.setTitle(role.name)
 				.setColor(role.color)
-				.addField("Position", `${role.position - 1}/${(int.guild as Guild).roles.cache.size - 1}`, true)
-				.addField(
-					"Created At",
-					`${discordTimestamp(role.createdTimestamp)} (${discordTimestamp(role.createdTimestamp, "R")})`,
-					true
-				)
+				.addField("Position", `${role.position + 1}/${(int.guild as Guild).roles.cache.size}`, true)
+				.addField("Created At", discordTimestamp(role.createdTimestamp, "R"), true)
+				.addField("Permissions", this.formatPermissions(role.permissions))
 				.setFooter(`ID: ${role.id}`);
 
-			return int.reply({ embeds: [embed] });
+			if (role.icon) embed.setThumbnail(role.iconURL()!);
+
+			return int.editReply({ embeds: [embed] });
 		} else if (subcommand === "server") {
-			const guild = int.guild!;
+			const guild = int.guild;
+			if (!guild?.available) {
+				return int.editReply({
+					embeds: [
+						new ErrorEmbed(
+							"Sorry, something went wrong while trying to get information about this guild. Please try again later; this shouldn't really happen!"
+						),
+					],
+				});
+			}
 
 			const embed = new MessageEmbed()
 				.setTitle(guild.name)
 				.setColor(guild.me!.roles.highest.color)
 				.addField("Member Count", guild.memberCount.toString(), true)
-				.addField(
-					"Created At",
-					`${discordTimestamp(guild.createdTimestamp)} (${discordTimestamp(guild.createdTimestamp)})`,
-					true
-				)
+				.addField("Created At", discordTimestamp(guild.createdTimestamp, "R"), true)
 				.setFooter(`ID: ${guild.id}`);
 
 			if (guild.icon) embed.setThumbnail(guild.iconURL({ dynamic: true, size: 256 })!);
+			if (guild.banner) embed.setImage(guild.bannerURL()!);
 
-			return int.reply({ embeds: [embed] });
+			return int.editReply({ embeds: [embed] });
 		}
+	}
+
+	private formatPermissions(permissions: Permissions) {
+		const permsArray = permissions.toArray();
+
+		if (permsArray.includes("ADMINISTRATOR")) {
+			return "`Administrator`";
+		}
+
+		return permissions
+			.toArray()
+			.map((p) => `\`${capitalizeFirstLetter(p.replace(/_/g, " "))}\` `)
+			.join(", ");
 	}
 }

178aed546ac55827a843feb7715a4660f294f201
BaumianerNiklas <68434250+BaumianerNiklas@users.noreply.github.com>
2022-07-18 21:56:09 +0200

    refactor(deploy): use APPLICATION_ID constant

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,10 +1,11 @@
+import { APPLICATION_ID } from "#util/constants.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 import { deployCommands } from "iubus";
 
 deployCommands({
-	applicationId: "723224456671002674",
+	applicationId: APPLICATION_ID,
 	token: process.env.BOT_TOKEN!,
 	commandDir: "dist/commands",
 	guildId: process.env.TESTING_GUILD_ID!,
